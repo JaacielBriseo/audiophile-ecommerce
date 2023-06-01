63e13fd568892acf4d2fe3043f0d6d6bef737a69
JaacielBriseo <jaacielbris23@gmail.com>
2023-05-31 18:26:26 -0700

    Initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		colors: {
+			orange: {
+				300: '#FBAF85',
+				600: '#D87D4A',
+			},
+			red: {
+				600: '#CD2C2C',
+			},
+			white: {
+				100: '#FFFFFF',
+				200: '#FAFAFA',
+				400: '#F1F1F1',
+			},
+			black: {
+				200: '#CFCFCF',
+				400: '#979797',
+				600: '#4c4c4c',
+				800: '#101010',
+				900: '#000000',
+			},
+		},
+		extend: {},
+	},
+	plugins: [],
+};
